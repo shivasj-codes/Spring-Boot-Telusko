4e9f4678505ec38f7ff46de0bbe4c7ef06d9f040
Shiva Sankar J <jshivasankar607@gmail.com>
2026-09-06 10:23:00 +0530

    Updated the code

---
+++ 2.32 Need of Array/NeedOfArray.java
@@ -0,0 +1,23 @@
+public class NeedOfArray {
+
+    // A normal variable can store only one value.
+    int i = 5;
+
+    // Another normal variable stores another value.
+    int j = 6;
+
+    int k = 7;
+
+    // An array can store multiple values in one variable.
+    // [] indicates that num is an array.
+    int num[] = { 5, 6, 7 };
+
+    // Creates an integer array with space for 4 values.
+    // new -> Creates a new array.
+    // int[4] -> The array size is 4.
+    //
+    // IMPORTANT:
+    // Once the array size is defined, its size is fixed.
+    int num1[] = new int[4];
+
+}
